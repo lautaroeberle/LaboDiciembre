f336ec3d8db043cf2195e90e90a0b8a2f4e06977
yo <105022957+lautaroeberle@users.noreply.github.com>
2023-12-02 11:51:22 -0300

    Delete Ejercicio456 directory

--- Ejercicio456/main.java
+++
@@ -1,9 +0,0 @@
-package Ejercicio456;
-
-import java.util.HashMap;
-
-public class main {
-    public static void main(String[] args) {
-        Dueño qq = new Dueño("Benicio","Sueldo",56128491);
-    }
-}
